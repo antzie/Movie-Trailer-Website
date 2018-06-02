bdd88f5ce33cd2c2d2ea7b58cf43250571799ad4
antzie <39847856+antzie@users.noreply.github.com>
2018-06-03 09:13:32 +1200

    Added Files to Repository

---
+++ entertainment_centre.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python2
+
+import fresh_tomatoes_ver3
+import media
+
+# Instances of Class Movie
+cinderella = media.Movie("Cinderella",
+                         "The classic fairytale told as it should be told",
+                         "https://upload.wikimedia.org/wikipedia/en/thumb/c/c2/Cinderella_2015_official_poster.jpg/220px-Cinderella_2015_official_poster.jpg",
+                         'https://www.youtube.com/watch?v=20DF6U1HcGQ',
+                         '84%',
+                         'Kenneth Branagh',
+                         'Cate Blanchett, Lily James, Richard Madden',
+                         2015
+                         )
+
+black_hawk_down = media.Movie("Black Hawk Down",
+                              "The Battle of Mogadishu, fought between US Special Forces and Somali militia fighters",
+                              'https://upload.wikimedia.org/wikipedia/en/thumb/0/0c/Black_hawk_down_ver1.jpg/220px-Black_hawk_down_ver1.jpg',
+                              "https://www.youtube.com/watch?v=5Y1ju8QwpQM",
+                              '76%',
+                              'Ridley Scott',
+                              'Josh Hartnett, Ewan McGregor, Eric Bana',
+                              2001
+                              )
+
+thor_ragnarok = media.Movie("Thor: Ragnarok",
+                            "Thor loses his hammer and has yet more family problems",
+                            "https://upload.wikimedia.org/wikipedia/en/7/7d/Thor_Ragnarok_poster.jpg",
+                            "https://www.youtube.com/watch?v=ue80QwXMRHg",
+                            '92%',
+                            'Taika Waititi',
+                            'Chris Hemsworth, Tom Hiddleston, Cate Blanchett',
+                            2017
+                            )
+
+darkest_hour = media.Movie("The Darkest Hour",
+                           "The story of Britain's leaders during the retreat to Dunkirk and the decisions they faced",
+                           "https://upload.wikimedia.org/wikipedia/en/3/38/Darkest_Hour_poster.png",
+                           "https://www.youtube.com/watch?v=LtJ60u7SUSw",
+                           '86%',
+                           'Joe Wright',
+                           'Gary Oldman, Kristin Scott Thomas, Lily James',
+                           2017
+                           )
+
+star_wars = media.Movie("Star Wars: A New Hope",
+                        "'You're classic when you start it all'",
+                        "https://upload.wikimedia.org/wikipedia/en/thumb/8/87/StarWarsMoviePoster1977.jpg/220px-StarWarsMoviePoster1977.jpg",
+                        "https://www.youtube.com/watch?v=MpkrMqmmy5k",
+                        '93%',
+                        'George Lucas',
+                        'Mark Hamill, Harrison Ford, Carrie Fisher',
+                        1977
+                        )
+
+the_man_from_nowhere = media.Movie("The Man From Nowhere",
+                                   "When a little girl is kidnapped by gangs, a mysterious pawn shop owner goes after her",
+                                   "https://upload.wikimedia.org/wikipedia/en/thumb/d/d6/The_Man_from_Nowhere_poster.jpg/220px-The_Man_from_Nowhere_poster.jpg",
+                                   "https://www.youtube.com/watch?v=38rPoGSr19U",
+                                   '100%',
+                                   'Lee Jeong-beom',
+                                   'Won Bin, Kim Sae-ron, Kim Tae-hoon',
+                                   2010
+                                   )
+
+# List of movie instances
+movies = [cinderella, black_hawk_down, the_man_from_nowhere,
+          darkest_hour, star_wars,  thor_ragnarok, ]
+
+# Create the movie-trailers web-page
+fresh_tomatoes_ver3.open_movies_page(movies)
---
+++ media.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python2
+# Version 03-06-2018
+
+import webbrowser
+
+
+class Movie():
+    """Stores Movie Related Information."""
+
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube, tomato_meter, movie_director,
+                 movie_actors, year_released):    
+        """Constructs a new instance of class Movie.
+
+        Args:
+                movie_title: the title of the relevant movie (String)
+                movie_storyline: the movie's storyline (String)
+                poster_image: the movie's poster (Url)
+                trailer_youtube: the movie's trailer - (Url) taken from Youtube.
+                tomato_meter: Rotten Tomatoes Ratings (String)
+                movie_director: the movie's director (String)
+                movie_actors: the movie's actors (String)
+                year_released: the year movie was released (Integer)
+        """
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+        self.rating = tomato_meter
+        self.director = movie_director
+        self.actors = movie_actors
+        self.year = year_released
+
+    def show_trailer(self):
+        """Opens the trailer for the relevant instance.
+
+        Opens in either a new window or a new tab.
+        Args:
+                self: an instance of class Movie
+        Returns:
+                the relevant trailer
+        """
+        return webbrowser.open(self.trailer_youtube_url)
